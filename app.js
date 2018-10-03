1262ae1c6e7a4ceb77ed7b6fd90096f052025661
Garry <yinfo@mail.ru>
2018-10-03 11:35:35 +0300

    Update app.js
    port 3000

--- app.js
+++ app.js
@@ -148,7 +148,7 @@ nconf.add('app', {type: 'file', file: config_app});
 
 // set app defaults
 var app_host = process.env.HOST || '0.0.0.0';
-var app_port = process.env.PORT || 1234;
+var app_port = process.env.PORT || 3000;
 
 // get the app configs and override if present
 if(nconf.stores.app.get('app:host') !== undefined){
